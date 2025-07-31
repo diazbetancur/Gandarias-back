510923c975dc7a14f4230b5f469d6a2ea452b3ec
John Alexander Diaz Betancur <diazbetancur@gmail.com>
2025-07-31 09:54:34 -0500

    restriccion hora de salida

--- CC.Domain/Dtos/UserShiftDto.cs
+++ CC.Domain/Dtos/UserShiftDto.cs
@@ -9,7 +9,7 @@ public class UserShiftDto
     public string Structure { get; set; }
     public DayOfWeek Day { get; set; }
     public TimeSpan Block1Start { get; set; }
-    public TimeSpan Block1lastStart { get; set; }
+    public TimeSpan? Block1lastStart { get; set; }
     public TimeSpan? Block2Start { get; set; }
     public TimeSpan? Block2lastStart { get; set; }
 }
